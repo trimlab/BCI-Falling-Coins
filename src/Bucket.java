660e05fca54c96daf92288c7533bb7b98c701fb8
Matthew Vaught <vaughtmt@gmail.com>
2014-11-12 11:07:17 -0500

    Bucket moves

--- src/Bucket.java
+++ src/Bucket.java
@@ -4,28 +4,29 @@ import java.awt.Graphics;
 
 public class Bucket {
 	private int x = 50;
-	private int y = 50;
-	private final int VELOCITY = 10;
+	private int y = 500;
+	private final int VELOCITY = 5;
 	private final int BASE_WIDTH = 60;
 	private final int INNER_WIDTH = 30;
-	private final int HEIGHT = 40;
+	public final int HEIGHT = 40;
 	private final int DEPTH = 25;
 	private final int TOP_WIDTH = 80;
 	private final int TOP_BASE_WIDTH_OFFSET;
 	private final int TOP_INNER_WIDTH_OFFSET;
 	private final int SCALE = 1;
+	public final int WIDTH;
+	private final int WINDOW_WIDTH;
 
-	public Bucket() {
+	public Bucket(int windowWidth) {
 		TOP_BASE_WIDTH_OFFSET = (TOP_WIDTH - BASE_WIDTH) / 2;
 		TOP_INNER_WIDTH_OFFSET = (TOP_WIDTH - INNER_WIDTH) / 2;
+		WIDTH = TOP_WIDTH;
+		WINDOW_WIDTH = windowWidth;
 	}
 
 	public void draw(Graphics g) {
 
 		g.setColor(Color.WHITE);
-
-		int x = 50;
-		int y = 50;
 
 		int[] xCord = { x, x + TOP_BASE_WIDTH_OFFSET * SCALE,
 				x + (TOP_WIDTH - TOP_BASE_WIDTH_OFFSET) * SCALE,
@@ -37,16 +38,23 @@ public class Bucket {
 				y + DEPTH * SCALE, y + DEPTH * SCALE };
 
 		g.fillPolygon(xCord, yCord, 6);
-
+		g.setColor(Color.BLUE);
+		g.fillRect(x, y, WIDTH, 2);
 
 	}
 
 	public void moveLeft() {
 		x -= VELOCITY;
+		if (x < 0) {
+			x = 0;
+		}
 	}
 
 	public void moveRight() {
 		x += VELOCITY;
+		if (x + WIDTH > WINDOW_WIDTH) {
+			x = WINDOW_WIDTH - WIDTH;
+		}
 	}
 
 	public int getX() {
--- src/MainGUI.java
+++ src/MainGUI.java
@@ -3,10 +3,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
@@ -18,6 +22,8 @@ public class MainGUI extends JFrame implements KeyListener {
 	private final Bucket BUCKET;
 	private final int PANEL_X = 640;
 	private final int PANEL_Y = 640;
+	private int motion = 0;
+	private boolean run = true;
 
 	/**
 	 * 
@@ -26,29 +32,82 @@ public class MainGUI extends JFrame implements KeyListener {
 
 	public MainGUI() {
 		super("BCI Falling Coins");
-		BUCKET = new Bucket();
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setMinimumSize(new Dimension(PANEL_X, PANEL_Y));
-		this.getContentPane().setBackground(Color.BLACK);
+		// this.setPreferredSize(new Dimension(PANEL_X + 10, PANEL_Y));
+		this.setBackground(Color.BLACK);
 		this.setVisible(true);
+		this.addKeyListener(this);
+
+
+		JPanel drawablePanel = new JPanel() {
+
+			@Override
+			public void paintComponent(Graphics g) {
+
+				Graphics2D g2D = (Graphics2D) g;
+				RenderingHints rh = new RenderingHints(
+						RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				g2D.setRenderingHints(rh);
+				BUCKET.draw(g);
+			}
+		};
+		drawablePanel.setOpaque(true);
+		this.add(drawablePanel);
+		// drawablePanel.setSize(new Dimension(PANEL_X, PANEL_Y));
+		BUCKET = new Bucket(PANEL_X);
+		Timer timer = createTimer();
+		timer.start();
 
 
 	}
 
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		Graphics2D g2D = (Graphics2D) g;
-		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		g2D.setRenderingHints(rh);
-		BUCKET.draw(g);
+	public void paintComponent(Graphics g) {
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		char test = e.getKeyChar();
+		// System.out.println(test);
+		if (test == 'a') {
+			motion = -1;
+		} else if (test == 's') {
+			motion = 0;
+		} else if (test == 'd') {
+			motion = 1;
+		}
 
+	}
+
+	private Timer createTimer() {
+		// Create the instructions that will execute every cycle
+		ActionListener actionListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				// checks to see if program is supposed to be running
+				if (run) {
+					// Redraw the window
+					// System.out.println(motion);
+					switch (motion) {
+					case -1:
+						// System.out.println("left");
+						BUCKET.moveLeft();
+						break;
+					case 0:
+						break;
+					case 1:
+						BUCKET.moveRight();
+						break;
+					}
+					repaint();
+				}
+			}
+		};
+		// Return the timer which ticks ~.005 s
+		return new Timer(10, actionListener);
 	}
 
 	@Override
@@ -60,6 +119,7 @@ public class MainGUI extends JFrame implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
+
 
 	}
 
